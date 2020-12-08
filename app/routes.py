ecdc0506f6546adebbf63bdc348800fab35c575a
Krunalpatel78 <75410831+Krunalpatel78@users.noreply.github.com>
2020-12-08 14:20:50 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -76,8 +76,6 @@ def webhook():
       return make_response('invalid params', 400)
   else: # POST
     body = json.loads(request.data)
-    Print ("Hi, This is webhoo")
-    Print (body)
-    #send_message(body)
+    send_message(body)
     return ("", 205)
 
